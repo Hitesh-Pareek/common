d7be96136bfda7641da2915a18636a076a2d849c
Hitesh <hiteshpareek23@gmail.com>
2024-07-08 15:36:11 +0530

    add

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import 'quasar/dist/quasar.css';
+
 import MyButton from './components/MyButton.vue';
+
+const app = createApp({});
+
+app.use(Quasar, {
+  config: {}
+});
+
+app.component('MyButton', MyButton);
 
 export { MyButton };
