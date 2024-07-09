cd5141c05069bb6805022043e9e507bc63a73924
Hitesh <hiteshpareek23@gmail.com>
2024-07-09 12:47:10 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import {
-  createApp
-} from 'vue';
-import {
   QBtn,
   Quasar
 } from 'quasar';
 import 'quasar/dist/quasar.css';
 import MyButton from './components/DemoComponent.vue';
 
-// Create a plugin to install Quasar and your components
-const install = (app) => {
-  app.use(Quasar, {
-    components: {
-      QBtn,
-      MyButton,
-    },
-  });
-
-  app.component('MyButton', MyButton);
-};
-
 export {
   MyButton,
-  install as
-  default
+  Quasar,
+  QBtn
 };
